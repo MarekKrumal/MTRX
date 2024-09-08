3b844fe34f1ad04728ae9162058eb80995444391
Marek Krumal <krumalmarek@gmail.com>
2024-09-08 07:34:02 +0200

    userController userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+
+
 
 dotenv.config();
 
@@ -8,5 +12,12 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//Routes
+app.use("/api/users",userRoutes)
 
 app.listen(5000, () => console.log(`Server started at http://localhost:${PORT}`));
