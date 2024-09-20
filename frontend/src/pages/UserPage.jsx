23dbb4c70fd8419209e486fd7169e62b666f314a
Marek Krumal <krumalmarek@gmail.com>
2024-09-21 01:43:49 +0200

    follow/ unfollow
    follow/ unfollow
    user Header and props
    callback for toast dependency error

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import UserHeader from "../components/UserHeader"
 import UserPost from "../components/UserPost"
+import { useParams } from "react-router-dom";
+import useShowToast from '../hooks/useShowToast';
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username }= useParams();
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getUser = async() => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`)
+        const data = await res.json()
+        if(data.error){
+          showToast("Error", data.error, "error")
+          return;
+        }
+        setUser(data);
+      } catch (error) {
+        showToast("Error", error, "error")
+      }
+    };
+
+    getUser();
+  },[username, showToast]);
+
+  if(!user) return null;
+
   return (
   <>
-    <UserHeader />
+    <UserHeader user={user} />
     <UserPost likes={22563} replies={895} postImg="/post1.png" postTitle="Lets talk about threads."/>
     <UserPost likes={8124} replies={705} postImg="/post2.jpg" postTitle="Nice place."/>
     <UserPost likes={1580} replies={896} postImg="/post3.png" postTitle="I love this guy."/>
