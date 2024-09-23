8999b599b7901e8e34cc75c42d935bf11e220fce
Marek Krumal <krumalmarek@gmail.com>
2024-09-23 21:42:57 +0200

    post /commnet funkcionalita real time vymazani

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -5,15 +5,17 @@ import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { formatDistanceToNow } from "date-fns";
 import { DeleteIcon } from "@chakra-ui/icons";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
+import postsAtom from "../atoms/postsAtom";
 
-const Post = ({ post, postedBy}) => {
+const Post = ({ post, postedBy }) => {
     
     const [user, setUser] = useState(null);
     const showToast = useShowToast();
     const navigate = useNavigate()
-    const currentUser = useRecoilValue(userAtom)
+    const currentUser = useRecoilValue(userAtom);
+    const [posts,setPosts] = useRecoilState(postsAtom);
 
     useEffect(() => {
         const getUser = async () => {
@@ -48,6 +50,7 @@ const Post = ({ post, postedBy}) => {
                 return;
             }
             showToast("Success", "Post deleted", "success");
+            setPosts(posts.filter((p) => p._id !== post._id));
         } catch (error) {
             showToast("Error", error.message, "error");
         }
@@ -141,7 +144,7 @@ const Post = ({ post, postedBy}) => {
             )}
 
             <Flex gap={3} my={1}>
-                <Actions post={post}/>
+                <Actions post={post} />
             </Flex>
 
             
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -6,13 +6,15 @@ import useShowToast from '../hooks/useShowToast';
 import { Flex, Spinner } from "@chakra-ui/react";
 import Post from "../components/Post";
 import useGetUserProfile from "../hooks/useGetUserProfile";
+import { useRecoilState } from "recoil";
+import postsAtom from "../atoms/postsAtom";
 
 const UserPage = () => {
   const {user,loading} = useGetUserProfile();
   const { username }= useParams();
   const showToast = useShowToast();
   
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useRecoilState(postsAtom);
   const [fetchingPosts, setFetchingPosts] = useState(true);
 
   useEffect(() => {
@@ -32,7 +34,8 @@ const UserPage = () => {
     }
 
     getPosts();
-  },[username, showToast]);
+  },[username, showToast, setPosts]);
+  console.log("posts is here and it is recoil state", posts);
 
   if(!user && loading) {        //nacitani spinner uprostred
     return (
@@ -55,7 +58,7 @@ const UserPage = () => {
 			)}
 
 			{posts.map((post) => (
-				<Post key={post._id} post={post} postedBy={post.postedBy} />
+				<Post key={post._id} post={post} postedBy={post.postedBy}/>
 			))}
     
   </>
